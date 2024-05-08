d235feed9d8b856a54b7ae299795c8305a79fc96
jvandinter <jipvandinter@hotmail.nl>
2024-05-08 16:36:31 +0200

    Rerun enrichment all cohorts

---
+++ 01_rnaseq/scripts/enrichment_total_cohort/create_count_matrix.R
@@ -0,0 +1,79 @@
+suppressPackageStartupMessages({
+library(DESeq2)
+library(magrittr)
+library(readr)
+})
+
+tumor_type="RMS"
+base_dir = "/hpc/pmc_vanheesch/projects/jvandinter/rms_analysis"
+rna_dir = paste(base_dir,"01_rnaseq", sep = "/")
+save_dir = paste(rna_dir,"analysis","quantification", sep = "/")
+tumor_txdb=paste0(rna_dir,"/analysis/rnaseq_pipeline/customannotation/",
+                 tumor_type,"_container/",
+                 tumor_type,"_full_novel_filtered_corrected.gtf_TxDb")
+
+# Create gene - transcript mappings
+txdb <- AnnotationDbi::loadDb(tumor_txdb)
+k <- keys(txdb, keytype = "TXNAME")
+tx2gene <- select(txdb, k, "GENEID", "TXNAME")
+
+meta_cohort <- read.delim(paste(rna_dir,"documentation/all_cohorts",
+                                "rms_meta_all_combined.csv", sep="/"), 
+                          sep = ",")
+
+print(paste(Sys.time(),"Finding salmon quant files ..."))
+
+# Load count files
+count_files <- list.files(paste(rna_dir,
+                                "analysis/quantification/salmon_quant",
+                                tumor_type), 
+                          recursive = T, 
+                          pattern = "quant.sf", 
+                          full.names = T)
+names(count_files) <- basename(base_dir(count_files))
+count_files <- count_files[which(names(count_files) %in% meta_cohort$sample_id)]
+
+print(paste(Sys.time(),"Loading salmon quant files ..."))
+
+txi <- tximport::tximport(count_files,
+                          type = "salmon",
+                          tx2gene = tx2gene,
+                          dropInfReps = T, 
+                          countsFromAbundance = "scaledTPM")
+
+print(paste(Sys.time(),"Loading salmon quant files. Done!"))
+
+saveRDS(txi, file = paste(save_dir, paste0(tumor_type,"_counts_all_cohorts.RDS"), sep = "/"))
+
+print(paste(Sys.time(),paste0("Count Matrix successfully created for ",tumor_type,"!")))
+
+  # Combine sample names per cohort
+  meta_cohort$cohort <- factor(meta_cohort$cohort, levels = unique(meta_cohort$cohort))
+  rownames(meta_cohort) <- meta_cohort$sample_id
+  samples <- meta_cohort$sample_id
+  print(nrow(meta_cohort))
+  
+  txi_subset <-
+    list(
+      abundance = txi$abundance[, which(colnames(txi$abundance) %in% samples)],
+      counts = txi$counts[, which(colnames(txi$counts) %in% samples)],
+      length = txi$length[, which(colnames(txi$length) %in% samples)],
+      countsFromAbundance = "scaledTPM"
+    )
+  
+  all(rownames(meta_cohort) == colnames(txi_subset$counts))
+  all(colnames(txi_subset$counts) == colnames(txi_subset$length))
+  
+  meta_cohort <- meta_cohort[colnames(txi_subset$counts),]
+  all(rownames(meta_cohort) == colnames(txi_subset$counts))
+
+  print(paste(Sys.time(),"Creating DEseq2 count matrix ..."))
+  
+  dds <- DESeq2::DESeqDataSetFromTximport(txi = txi_subset,
+                                               colData = meta_cohort,
+                                               design = ~ 1)
+
+  saveRDS(dds,
+          file = paste(save_dir,"RMS_all_cohorts_for_plotting.RDS", sep = "/"))
+
+  print(paste(Sys.time(),paste0("Count Matrix successfully created for ",tumor_type,"!")))
---
+++ 01_rnaseq/scripts/enrichment_total_cohort/create_count_matrix.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name create_count_matrix
+#SBATCH --mem=120G
+#SBATCH --time=12:00:00
+#SBATCH --gres=tmpspace:10G
+
+version="4.3.0"
+
+# Create temporary directory to be populated with directories to bind-mount in the container
+# where writable file systems are necessary. Adjust path as appropriate for your computing environment.
+singularity_dir="/hpc/local/Rocky8/pmc_vanheesch/singularity_images"
+wd="/hpc/pmc_vanheesch/projects/jvandinter/rms_analysis/01_rnaseq"
+workdir=${TMPDIR}
+
+mkdir -p -m 700 ${workdir}/run ${workdir}/tmp ${workdir}/var/lib/rstudio-server
+cat > ${workdir}/database.conf <<END
+provider=sqlite
+directory=/var/lib/rstudio-server
+END
+
+# Set OMP_NUM_THREADS to prevent OpenBLAS (and any other OpenMP-enhanced
+# libraries used by R) from spawning more threads than the number of processors
+# allocated to the job.
+#
+# Set R_LIBS_USER to a path specific to rocker/rstudio to avoid conflicts with
+# personal libraries from any R installation in the host environment
+
+cat > ${workdir}/rsession.sh <<END
+#!/bin/sh
+export OMP_NUM_THREADS=${SLURM_JOB_CPUS_PER_NODE}
+export R_LIBS_USER=/hpc/local/Rocky8/pmc_vanheesch/Rstudio_Server_Libs/Rstudio_${version}_libs
+exec /usr/lib/rstudio-server/bin/rsession "\${@}"
+END
+
+chmod +x ${workdir}/rsession.sh
+
+export APPTAINER_BIND="${workdir}/run:/run,${workdir}/tmp:/tmp,${workdir}/database.conf:/etc/rstudio/database.conf,${workdir}/rsession.sh:/etc/rstudio/rsession.sh,${workdir}/var/lib/rstudio-server:/var/lib/rstudio-server,/hpc/pmc_vanheesch,/hpc/local/Rocky8/pmc_vanheesch/Rstudio_Server_Libs/Rstudio_${version}_libs:/usr/local/lib/R/site-library"
+
+# Do not suspend idle sessions.
+# Alternative to setting session-timeout-minutes=0 in /etc/rstudio/rsession.conf
+# https://github.com/rstudio/rstudio/blob/v1.4.1106/src/cpp/server/ServerSessionManager.cpp#L126
+export APPTAINERENV_RSTUDIO_SESSION_TIMEOUT=0
+
+apptainer exec --cleanenv ${singularity_dir}/rstudio_${version}_bioconductor.sif \
+  Rscript ${wd}/scripts/enrichment_total_cohort/create_count_matrix.R
